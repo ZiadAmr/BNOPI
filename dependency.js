1daab06e7eb7bf54e4f245fe25f10fc471f1e76f
unknown <ziad2ziad2@gmail.com>
2022-11-16 18:27:48 +0000

    Wrote Dependency graph structure, can now create graph and execute it, still needs testing

--- dependency.js
+++ dependency.js
@@ -1,3 +1,33 @@
+dpgraph = []
+
+function addStage(stageName, filename, parameters=[], parents=[]) {
+    dpgraph.push({
+        "name": stageName,
+        "file": filename,
+        "params": parameters,
+        "parents": parents
+    })
+}
+
+function runGraph() {
+    stages_run = []
+    while(stages_run.length < dpgraph.length){
+        run = []
+        dpgraph.forEach((x, i) => {
+            if( x.parents.every( p => stages_run.find(e => e == p)) && x.name != stages_run){
+                run.push(x)
+            }
+        });
+        // console.log(run)
+        run.forEach((x,i) => runScript(x.file))
+        run.forEach((x,i) => stages_run.push(x.name))
+        console.log("loop", stages_run)
+        // console.log("Stages Run: ", stages_run)
+    }
+    
+    console.log("Stages Run: ", stages_run)
+}
+
 async function runScript(filename) {
     /*...*/
     var python = require('child_process').spawn('python', [filename]);
@@ -7,5 +37,11 @@ async function runScript(filename) {
     });
 }
 
-runScript('python-scripts/example.py')
+// runScript('python-scripts/example.py')
 
+addStage("example", "python-scripts/example.py")
+addStage("secondexample", "python-scripts/example.py", [], ["example"])
+
+runGraph()
+
+// console.log(dpgraph)
