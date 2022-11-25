ab5f7094d8fe80ed2463420c3314b66af85a6386
unknown <ziad2ziad2@gmail.com>
2022-11-25 06:11:00 +0000

    Can now pass arguments to nodes and save their outputs in memory under the stage name

--- dependency.js
+++ dependency.js
@@ -1,4 +1,8 @@
+const { memo } = require('react')
+
 dpgraph = []
+
+memory = {}
 
 function addStage(stageName, filename, parameters=[], parents=[]) {
     dpgraph.push({
@@ -7,6 +11,10 @@ function addStage(stageName, filename, parameters=[], parents=[]) {
         "params": parameters,
         "parents": parents
     })
+}
+
+function addParam(name, value) {
+    memory[name] = value
 }
 
 function runGraph() {
@@ -19,7 +27,7 @@ function runGraph() {
             }
         });
         // console.log(run)
-        run.forEach((x,i) => runScript(x.file))
+        run.forEach((x,i) => runScript(x.name, x.file, x.params))
         run.forEach((x,i) => stages_run.push(x.name))
         console.log("loop", stages_run)
         // console.log("Stages Run: ", stages_run)
@@ -28,11 +36,21 @@ function runGraph() {
     console.log("Stages Run: ", stages_run)
 }
 
-async function runScript(filename) {
+function runScript(stageName, filename, params) {
     /*...*/
-    var python = require('child_process').spawn('python', [filename]);
+    args = [filename]
+    console.log("Params = ", params)
+    for (var i = 0; i < params.length; i++){
+        args.push(memory[params[i]])
+    }
+    console.log(args, memory["param1"], memory["param2"])
+    var python = require('child_process').spawn('python', args);
     python.stdout.on('data',function(data){
         console.log("data: ",data.toString());
+        memory[stageName] = data.toString()
+        
+        console.log("after:", memory)
+
         return data.toString()
     });
 }
@@ -40,8 +58,10 @@ async function runScript(filename) {
 // runScript('python-scripts/example.py')
 
 addStage("example", "python-scripts/example.py")
-addStage("secondexample", "python-scripts/example.py", [], ["example"])
+
+addStage("secondexample", "python-scripts/io.py", ["param1", "param2"], ["example"])
+addParam("param1", 1)
+addParam("param2", "food")
 
 runGraph()
 
-// console.log(dpgraph)
