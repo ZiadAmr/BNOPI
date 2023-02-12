f547284f3c2214b8927211a294b0cc0dff654262
Tom Peach <thomasopeach@btinternet.com>
2023-02-12 16:11:38 +0000

    common update

--- algs/genetic-alg/src/common.hpp
+++ algs/genetic-alg/src/common.hpp
@@ -2,10 +2,12 @@
 #include <vector>
 #include "link.hpp"
 
-typedef std::vector<std::vector<Link>> RouteNet;
+typedef std::vector<Link*> Route;
+typedef std::vector<Route> RouteNet;
 
 struct AlgSettings
 {
 	int max_route_stops;
+	int min_route_stops;
 	int num_routes;
 };
